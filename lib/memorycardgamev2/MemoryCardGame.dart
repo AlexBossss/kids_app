d7182441699c6c58facc46133f8d6cc0eb36ef0f
alexbossss <alexperminov94@gmail.com>
2020-05-16 19:44:07 +0400

    add: duration before toggle (correct2)

--- lib/memorycardgamev2/MemoryCardGame.dart
+++ lib/memorycardgamev2/MemoryCardGame.dart
@@ -143,50 +143,48 @@ class _MemoryGameState extends State<MemoryGame> {
                             ? FlipCard(
                                 key: _cardStateKeys[index],
                                 onFlip: () {
-                                  if(!_wait) {
-                                    if (!_flip) {
-                                      _flip = true;
-                                      _previousIndex = index;
-                                    } else {
-                                      _flip = false;
-                                      if (_previousIndex != index) {
-                                        if (_data[_previousIndex] !=
-                                            _data[index]) {
-                                          _wait = true;
+                                  if (!_flip) {
+                                    _flip = true;
+                                    _previousIndex = index;
+                                  } else {
+                                    _flip = false;
+                                    if (_previousIndex != index) {
+                                      if (_data[_previousIndex] !=
+                                          _data[index]) {
+                                        _wait = true;
+                                        Future.delayed(
+                                            const Duration(milliseconds: 1200),
+                                            () {
+                                          _cardStateKeys[_previousIndex]
+                                              .currentState
+                                              .toggleCard();
+                                          _cardStateKeys[index]
+                                              .currentState
+                                              .toggleCard();
+                                          _previousIndex = index;
+                                          _wait = false;
+                                        });
+                                      } else {
+                                        _cardFlips[_previousIndex] = false;
+                                        _cardFlips[index] = false;
+                                        print(_cardFlips);
+                                        if (_cardFlips
+                                            .every((t) => t == false)) {
+                                          print("Won");
                                           Future.delayed(
-                                              const Duration(
-                                                  milliseconds: 1200), () {
-                                            _cardStateKeys[_previousIndex]
-                                                .currentState
-                                                .toggleCard();
-                                            _cardStateKeys[index]
-                                                .currentState
-                                                .toggleCard();
-                                            _previousIndex = index;
-                                            _wait=false;
-                                          });
-                                        } else {
-                                          _cardFlips[_previousIndex] = false;
-                                          _cardFlips[index] = false;
-                                          print(_cardFlips);
-                                          if (_cardFlips
-                                              .every((t) => t == false)) {
-                                            print("Won");
-                                            Future.delayed(
-                                                const Duration(seconds: 1), () {
-                                              setState(() {
-                                                _isFinished = true;
-                                                _start = false;
-                                              });
+                                              const Duration(seconds: 1), () {
+                                            setState(() {
+                                              _isFinished = true;
+                                              _start = false;
                                             });
-                                          }
+                                          });
                                         }
                                       }
                                     }
                                   }
                                   setState(() {});
                                 },
-                                flipOnTouch: _wait? false: _cardFlips[index],
+                                flipOnTouch: _wait ? false : _cardFlips[index],
                                 direction: FlipDirection.HORIZONTAL,
                                 front: Container(
                                   margin: EdgeInsets.all(4.0),
