fac62e8ee290d7382f2a8c1809f0b9853c931421
alexbossss <alexperminov94@gmail.com>
2020-05-25 19:58:27 +0400

    add: restart button

--- lib/home/homapages/DeductiveGamePage.dart
+++ lib/home/homapages/DeductiveGamePage.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kidsapp/models/TabBarPage.dart';
-
 import 'dedgameslists/ConsiderationList.dart';
 import 'dedgameslists/LogicList.dart';
 import 'dedgameslists/MemoryList.dart';
 
 class DeductiveGamePage extends StatelessWidget {
   final String _title = 'D Games';
-  final List<MaterialColor> _color = [Colors.blue, Colors.purple, Colors.red];
+  final Color _color = Colors.transparent;
   final _dGameTabs = <Tab>[
-    Tab(text: 'Memory'),
-    Tab(text: 'Logic'),
-    Tab(text: 'Consideration'),
+    Tab(
+        child: Text('Memory',
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w800))),
+    Tab(
+        child: Text('Logic',
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w800))),
+    Tab(
+        child: Text('Attention',
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w800))),
   ];
   final _dGamePages = [MemoryList(), LogicList(), ConsiderationList()];
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:kidsapp/games/remembercardgame/RememberCard.dart';
 import 'package:kidsapp/home/HomeScreen.dart';
 import 'games/memorycardgame/MemoryCardGameMenu.dart';
+import 'package:kidsapp/src/theme/theme.dart';
 
 //super app next 1
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the of your application.
+  // This widget is the of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/': (BuildContext context) => HomeScreen(),
         '/memoryCardGame' : (BuildContext context) => MemoryCardGameMenu(),
+        '/rememberCard' : (BuildContext context) => RememberCard(),
       },
-    ),
-  );
+    );
+  }
 }
