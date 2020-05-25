5e02b69146724b8f97b530969986fc20a0ad28a7
andrsin23 <art-maker@yandex.ru>
2020-05-26 00:10:24 +0300

    change Switch to ToggleButtons

--- lib/games/memorycardgame/MemoryCardGameMenu.dart
+++ lib/games/memorycardgame/MemoryCardGameMenu.dart
@@ -40,26 +40,30 @@ class _MenuState extends State<MemoryCardGameMenu> {
       .toList();
 
   Level checkLevel() {
-    if (_isHard == true) {
+    if (isSelected[2] == true) {
       return Level.Hard;
-    } else if (_isMid == true) {
+    } else if (isSelected[1] == true) {
       return Level.Medium;
     } else {
       return Level.Easy;
     }
   }
 
+  List<bool> isSelected;
+
   @override
   void initState() {
+    isSelected = [true, false, false];
     super.initState();
-    _isHard = false;
-    _isMid = false;
-    _isEasy = true;
+    // _isHard = false;
+    // _isMid = false;
+    // _isEasy = true;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.green,
       body: SafeArea(
         child: Center(
           child: Column(
@@ -74,50 +78,84 @@ class _MenuState extends State<MemoryCardGameMenu> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text(Level.Hard.toString().split('.').last),
-                  Switch(
-                    onChanged: (bool value) {
-                      setState(() => {
-                            this._isHard = value,
-                            this._isEasy = false,
-                            this._isMid = false
-                          });
+                  ToggleButtons(
+                    selectedColor: Colors.orange,
+                    children: <Widget>[
+                      Icon(
+                        Icons.star_half,
+                        size: 80.0,
+                      ),
+                      Icon(
+                        Icons.star,
+                        size: 80.0,
+                      ),
+                      Icon(
+                        Icons.stars,
+                        size: 80.0,
+                      ),
+                    ],
+                    onPressed: (int index) {
+                      setState(() {
+                        for (int buttonIndex = 0;
+                            buttonIndex < isSelected.length;
+                            buttonIndex++) {
+                          if (buttonIndex == index) {
+                            isSelected[buttonIndex] = true;
+                          } else {
+                            isSelected[buttonIndex] = false;
+                          }
+                        }
+                      });
                     },
-                    value: this._isHard,
+                    isSelected: isSelected,
                   ),
                 ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(Level.Medium.toString().split('.').last),
-                  Switch(
-                    onChanged: (bool value) {
-                      setState(() => {
-                            this._isMid = value,
-                            this._isHard = false,
-                            this._isEasy = false
-                          });
-                    },
-                    value: this._isMid,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(Level.Easy.toString().split('.').last),
-                  Switch(
-                    onChanged: (bool value) {
-                      setState(() => {
-                            this._isEasy = value,
-                            this._isHard = false,
-                            this._isMid = false
-                          });
-                    },
-                    value: this._isEasy,
-                  ),
-                ],
+
+                //   <Widget>[
+                //     Text(Level.Hard.toString().split('.').last),
+                //     Switch(
+                //       onChanged: (bool value) {
+                //         setState(() => {
+                //               this._isHard = value,
+                //               this._isEasy = false,
+                //               this._isMid = false
+                //             });
+                //       },
+                //       value: this._isHard,
+                //     ),
+                //   ],
+                // ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: <Widget>[
+                //     Text(Level.Medium.toString().split('.').last),
+                //     Switch(
+                //       onChanged: (bool value) {
+                //         setState(() => {
+                //               this._isMid = value,
+                //               this._isHard = false,
+                //               this._isEasy = false
+                //             });
+                //       },
+                //       value: this._isMid,
+                //     ),
+                //   ],
+                // ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: <Widget>[
+                //     Text(Level.Easy.toString().split('.').last),
+                //     Switch(
+                //       onChanged: (bool value) {
+                //         setState(() => {
+                //               this._isEasy = value,
+                //               this._isHard = false,
+                //               this._isMid = false
+                //             });
+                //       },
+                //       value: this._isEasy,
+                //     ),
+                //   ],
               ),
               ListTile(
                 title: Text('Card:'),
@@ -140,21 +178,22 @@ class _MenuState extends State<MemoryCardGameMenu> {
                     Navigator.push(
                         context,
                         PageRouteBuilder(
-                          transitionDuration: Duration(seconds:1),
-                          transitionsBuilder: (BuildContext context,
-                          Animation<double> animation,
-                          Animation<double> secAnimation,
-                          Widget child){
-                            return ScaleTransition(
-                              alignment: Alignment.center,
-                              scale: animation,
-                              child: child,
-                            );
-                          },
-                          pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secAnimation){
-                            return MemoryGame(_level,_kind);
-                          }
-                        ));
+                            transitionDuration: Duration(seconds: 1),
+                            transitionsBuilder: (BuildContext context,
+                                Animation<double> animation,
+                                Animation<double> secAnimation,
+                                Widget child) {
+                              return ScaleTransition(
+                                alignment: Alignment.center,
+                                scale: animation,
+                                child: child,
+                              );
+                            },
+                            pageBuilder: (BuildContext context,
+                                Animation<double> animation,
+                                Animation<double> secAnimation) {
+                              return MemoryGame(_level, _kind);
+                            }));
                     print(_kindString);
                     print(_level);
                   }),
