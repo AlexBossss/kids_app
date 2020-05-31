235a77db37438156ca8d168ad7910b7adea8ff2d
alexbossss <alexperminov94@gmail.com>
2020-05-31 20:41:11 +0400

    add: remember one item game round

--- lib/games/rememberoneitem/RememberOneItemRound.dart
+++ lib/games/rememberoneitem/RememberOneItemRound.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flip_card/flip_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -11,16 +13,55 @@ class RememberOneItemRound extends StatefulWidget {
 class _RememberOneItemRoundState extends State<RememberOneItemRound> {
   List<String> _data;
   List<GlobalKey<FlipCardState>> _cardStateKeys;
+  List<String> _answers;
+  double _buttonHeight = 50;
+  double _topPaddingToAnimation = 400;
+  double _lefPadding = 0;
+  double _heightAnswers = 0;
+  bool _isDone;
 
+
+  int _timeForTopPadding = 800;
   @override
   void initState() {
+    _isDone = false;
     _cardStateKeys = getKeys();
     _data = getData();
-//    _random = Random();
+    _random = Random().nextInt(4);
+    _answers = getAnswers();
     super.initState();
   }
 
-//  int _random;
+  List getAnswers() {
+    List answers = getData();
+    answers.forEach((element) {
+      while (element == _data[_random]) {
+        answers = getData();
+      }
+    });
+    answers[_random] = _data[_random];
+    answers.shuffle();
+    return answers;
+  }
+
+  void removeButton() {
+    setState(() {
+      _buttonHeight = 0;
+      _topPaddingToAnimation = 50;
+      _heightAnswers = 90;
+    });
+  }
+
+  void removeAnswers() {
+    Future.delayed(Duration(milliseconds: 900), () {
+      setState(() {
+        _lefPadding = 500;
+        _timeForTopPadding = 200;
+      });
+    });
+  }
+
+  int _random;
 
   @override
   Widget build(BuildContext context) {
@@ -30,23 +71,56 @@ class _RememberOneItemRoundState extends State<RememberOneItemRound> {
           Wrap(
             children: _data
                 .map((e) => Container(
-                    height: 100,
+                    height: 90,
                     child: FlipCard(
+                        flipOnTouch: false,
                         key: _cardStateKeys[_data.indexOf(e)],
                         front: Image.asset(e),
                         back: Image.asset(missing))))
                 .toList(),
           ),
           GestureDetector(
-            onTap:() => _cardStateKeys[1].currentState.toggleCard(),
-            child: Container(
+            onTap: () => {
+              _cardStateKeys[_random].currentState.toggleCard(),
+              print(_random),
+              removeButton()
+            },
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 200),
               alignment: Alignment.center,
-              height: 50,
+              height: _buttonHeight,
               width: 200,
               child: Text('Pidr'),
               color: Colors.green,
             ),
-          )
+          ),
+          AnimatedPadding(
+            padding:
+                EdgeInsets.only(top: _topPaddingToAnimation, left: _lefPadding),
+            duration: Duration(milliseconds: _timeForTopPadding),
+            child: AnimatedContainer(
+              height: _heightAnswers,
+              curve: Curves.bounceOut,
+              duration: Duration(milliseconds: 200),
+              child: Wrap(
+                children: _answers
+                    .map((e) => GestureDetector(
+                          onTap: () {
+                            if (e == _data[_random] && !_isDone) {
+                              _cardStateKeys[_random].currentState.toggleCard();
+                              _isDone = true;
+                              removeAnswers();
+                            }
+                          },
+                          child: Container(
+                            child: Image.asset(e),
+                            height: 90,
+                          ),
+                        ))
+                    .toList(),
+              ),
+            ),
+          ),
         ],
       ),
     );
